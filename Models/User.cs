ba716ecec1b112ff218c6997d303dffad94cbc27
cavalenciad <cavalenciad@outlook.com>
2025-05-05 18:22:03 -0500

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Amadeus.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+    }
+}
